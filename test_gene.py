1e6df430900ef06cf624a6f0885676551fcff83d
Anastasiya <anastasiavorobeva029@gmail.com>
2026-06-19 22:05:54 +0400

    test_project 19.06

--- test_gene.py
+++ test_gene.py
@@ -1,3 +1,11 @@
 from Bio.Seq import Seq
 from Bio import Restriction
+import pandas as pd
+import numpy as np
+
+
+def read_fasta_file():
+
+    file_path = 'C:\Users\Nastya\PycharmProjects\plasmida_project\data\ncbi_dataset\data\gene.fna'
+
 
