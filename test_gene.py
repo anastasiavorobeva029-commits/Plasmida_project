6840cd1382249db53336f1a722191aa02ddea1b7
Anastasiya <anastasiavorobeva029@gmail.com>
2026-06-19 22:19:54 +0400

    test_project 19.06

--- test_gene.py
+++ test_gene.py
@@ -1,11 +1,32 @@
-from Bio.Seq import Seq
-from Bio import Restriction
 import pandas as pd
 import numpy as np
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio import Restriction
 
 
-def read_fasta_file():
+def read_fasta_file(file_path):
+    sequences = []
 
-    file_path = 'C:\Users\Nastya\PycharmProjects\plasmida_project\data\ncbi_dataset\data\gene.fna'
+    for record in SeqIO.parse(file_path, 'fasta'):
+        sequences.append({
+            'id': record.id,
+            'description': record.description,
+            'sequence': str(record.seq),
+            'length': len(record.seq)
+        })
+
+    df = pd.DataFrame(sequences)
+    return df
+
+file_path = r'C:\Users\Nastya\PycharmProjects\plasmida_project\data\ncbi_dataset\data\pUC19_sequence.fasta'
+df = read_fasta_file(file_path)
+
+print(df)
+print(f"Найдено {len(df)} последовательностей")
+
+
+
+
 
 
